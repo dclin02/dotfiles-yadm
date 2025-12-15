4ec13e560c4f17654bcad6016091cc5d69e3eba6
Daniel Lin <dclin02@gmail.com>
2025-12-14 21:21:25 -0300

    add LazyVim config

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add onedarker
+  { "lunarvim/onedarker.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedarker",
+    },
+  },
+}
