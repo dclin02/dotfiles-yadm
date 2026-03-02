50696eec9bd7a4f83e07ed002fb2dde6fb3a7abb
Daniel Lin <dclin02@gmail.com>
2026-03-02 13:53:09 -0300

    monitor script: change output codes after dockstation

--- .screenlayout/monitor.sh
+++ .screenlayout/monitor.sh
@@ -7,11 +7,19 @@ xrandr \
   # --output HDMI-A-0 --mode 1920x1080 --left-of DisplayPort-1 --rotate left \
   # --output HDMI-A-1-1 --mode 1920x1080 --right-of DisplayPort-1 --rotate right
   sleep 1 &&
+  # xrandr \
+  #   --output eDP-1 --mode 1920x1200 --rate 60 --pos 1595x1480 --rotate normal --scale 0.80x0.80 \
+  #   --output HDMI-1 --primary --mode 2560x1440 --rate 120 --pos 1080x40 --rotate normal \
+  #   --output DP-1 --rate 120 --mode 1920x1080 --pos 0x0 --rotate left \
+  #   --output DP-3 --rate 75 --mode 1920x1080 --pos 3640x0 --rotate right
   xrandr \
     --output eDP-1 --mode 1920x1200 --rate 60 --pos 1595x1480 --rotate normal --scale 0.80x0.80 \
-    --output HDMI-1 --primary --mode 2560x1440 --rate 120 --pos 1080x40 --rotate normal \
-    --output DP-1 --rate 120 --mode 1920x1080 --pos 0x0 --rotate left \
-    --output DP-3 --rate 75 --mode 1920x1080 --pos 3640x0 --rotate right
+    --output DP-1-2 --primary --mode 2560x1440 --rate 120 --pos 1080x40 --rotate normal \
+    --output DP-1-1 --rate 120 --mode 1920x1080 --pos 0x0 --rotate left \
+    --output DP-1-3 --rate 75 --mode 1920x1080 --pos 3640x0 --rotate right
+# xrandr --output eDP-1 --mode 1920x1200 --pos 456x1440 --rotate normal \
+#   --output HDMI-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+
 # xrandr \
 #   --output DisplayPort-1 --primary --rate 240 --mode 2560x1440 --pos 1080x132 --rotate normal \
 #   --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate left \
